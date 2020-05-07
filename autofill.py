6e897f66c24d1c0f702bd33cfd42b18389e06d5a
Nicholas de Paola <ndepaola97@gmail.com>
2020-05-07 22:04:21 +1000

    improved speed and reliability

--- autofill.py
+++ autofill.py
@@ -1,10 +1,13 @@
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 import time
 import os
 import xml.etree.ElementTree as ET
+from selenium.webdriver.support.expected_conditions import invisibility_of_element
+from selenium.webdriver.support.ui import WebDriverWait
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 """
@@ -12,8 +15,6 @@ TODO:
 - Handle the popup "your internet connection is slow so this'll take a while
 - Redownload images if they happen to corrupt while downloading, which currently stops the script
 - Download google drive images in a separate process while cards are being uploaded and inserted into their correct slots
-- Improve the speed of dragging and dropping while keeping its current reliability
-- Pause for only as long as is necessary on the various menus that currently have 10 second delays
 - Would be nice to have a way of deselecting text bc sometimes the script selects a bunch of content on the page and it's slightly ugly
 """
 
@@ -40,7 +41,8 @@ def fill_cards():
         qty_dropdown.select_by_value(root[0][1].text)
 
         # Accept current settings and move to next step
-        driver.execute_script("javascript:doPersonalize('https://www.makeplayingcards.com/products/pro_item_process_flow.aspx')")
+        driver.execute_script(
+            "javascript:doPersonalize('https://www.makeplayingcards.com/products/pro_item_process_flow.aspx')")
 
         # Key in the desired number of cards, then move to the next step
         driver.switch_to.frame("sysifm_loginFrame")
@@ -53,8 +55,25 @@ def fill_cards():
         driver.switch_to.default_content()
 
         # Insert card fronts
-        for card in root[1]:
-            upload_and_insert_card(driver, card[0].text, card[1].text.strip('][').replace(" ", "").split(','))
+        fronts_slot = [x[1].text.strip("][").replace(" ", "").split(",") for x in root[1]]
+        autofillBtn = driver.find_element_by_class_name("autoFill")
+        for i in range(0, len(root[1])):
+            elem = upload_card(driver, root[1][i][0].text)
+            try:
+                if len(fronts_slot[i]) > 1:
+                    for slot in fronts_slot[i]:
+                        # drag and drop card into each slot
+                        drag_drop_card(driver, elem, slot)
+                elif len(fronts_slot[i+1]) > 1:
+                    # click autofill button
+                    autofillBtn.click()
+                    wait(driver)
+            except IndexError:
+                # click autofill button
+                autofillBtn.click()
+                wait(driver)
+
+            time.sleep(0.3)
 
         # Page through to backs
         driver.execute_script("javascript:oDesign.setNextStep();")
@@ -63,20 +82,22 @@ def fill_cards():
             alert.accept()
         except NoAlertPresentException:
             pass
-        time.sleep(10)  # TODO: Variable delay
+
+        # Page over to the next step from "add text to fronts"
+        wait(driver)
         driver.execute_script("javascript:oDesign.setNextStep();")
 
         # Select "different images" for backs
-        time.sleep(5)  # TODO: Variable delay
+        wait(driver)
         driver.switch_to.frame("sysifm_loginFrame")
-        qtyField = driver.find_element_by_id("txt_card_number")
 
         if len(root[2]) == 0:
             # Same cardback for every card
             driver.execute_script("javascript:setMode('ImageText', 1);")
             driver.switch_to.default_content()
-
-            upload_and_insert_card(driver, root[3].text, [0])
+            autofillBtn = driver.find_element_by_class_name("autoFill")
+            upload_card(driver, root[3].text)
+            autofillBtn.click()
         else:
             # Different cardbacks
             driver.execute_script("javascript:setMode('ImageText', 0);")
@@ -102,26 +123,42 @@ def fill_cards():
             alert.accept()
         except NoAlertPresentException:
             pass
-        time.sleep(10)  # TODO: Variable delay
+
+        wait(driver)
         driver.execute_script("javascript:oDesign.setNextStep();")
 
         input("Press enter to finish up.")
 
 
-def upload_and_insert_card(driver, drive_id, slots):
+def wait(driver):
+    try:
+        wait_elem = driver.find_element_by_id("sysimg_wait")
+        WebDriverWait(driver, 100).until(invisibility_of_element(wait_elem))
+    except NoSuchElementException:
+        return
+
+
+def upload_card(driver, drive_id):
     filepath = os.getcwd() + "\cards\{}.png".format(drive_id)
     gdd.download_file_from_google_drive(file_id=drive_id,
                                         dest_path=filepath)
 
     num_elems = len(driver.find_elements_by_xpath("//*[contains(@id, 'upload_')]"))
-    driver.find_element_by_xpath('//*[@id="uploadId"]').send_keys(filepath)  
+    driver.find_element_by_xpath('//*[@id="uploadId"]').send_keys(filepath)
 
     while True:
         # Wait until the image has finished uploading
         elem = driver.find_elements_by_xpath("//*[contains(@id, 'upload_')]")
         if len(elem) > num_elems:
             print("New card uploaded")
-            break
+            time.sleep(1)
+            return elem[-1]
+
+
+def upload_and_insert_card(driver, drive_id, slots):
+    elem = upload_card(driver, drive_id)
+    if type(elem) != list:
+        elem = [elem]
 
     # Insert card into the appropriate slots
     time.sleep(1)
@@ -135,12 +172,11 @@ def drag_drop_card(driver, cardElement, slotNumber):
     current_y = elem_slot.location['y']
     driver.execute_script("arguments[0].scrollIntoView();", elem_slot)
     ActionChains(driver).click_and_hold(cardElement).move_to_element(elem_slot).release(elem_slot).perform()
+    wait(driver)
     while driver.find_element_by_id("dnImg{}_0_0".format(slotNumber)) is None:
-        time.sleep(0.5)
-    while elem_visible.value_of_css_property("display") == "none":
+        time.sleep(0.3)
         ActionChains(driver).click_and_hold(cardElement).move_to_element(elem_slot).release(elem_slot).perform()
-        time.sleep(0.5)
-    time.sleep(0.3)
+        wait(driver)
 
 
 if __name__ == "__main__":
