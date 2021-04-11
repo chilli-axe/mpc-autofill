0e08cc58acae8be9aa8b87f72b6b9d2f072db8cb
ndepaola <ndepaola97@gmail.com>
2021-04-11 13:26:44 +1000

    improved deployment process for google analytics

--- MPCAutofill/MPCAutofill/settings.py
+++ MPCAutofill/MPCAutofill/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cardpicker.context_processors.add_gtag',
             ],
         },
     },
---
+++ MPCAutofill/cardpicker/context_processors.py
@@ -0,0 +1,9 @@
+try:
+    from ga_settings import GTAG
+except ImportError:
+    GTAG = ""
+
+def add_gtag(request):
+    return {
+        'GTAG': GTAG
+    }
