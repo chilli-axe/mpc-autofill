73c3dc6bf29df18739254218d44837864957e400
Nicholas de Paola <ndepaola97@gmail.com>
2021-06-02 21:48:04 +1000

    improved the admin interface a bit
    couldn't find a way to add buttons to run management commands though - maybe more research is required?

--- MPCAutofill/cardpicker/admin.py
+++ MPCAutofill/cardpicker/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
-from .models import Card, Cardback, Token, Source, DFCPair
+
+from .models import Card, Cardback, DFCPair, Source, Token
+
 
 # Register your models here.
-admin.site.register(Card)
-admin.site.register(Cardback)
-admin.site.register(Token)
-admin.site.register(Source)
-admin.site.register(DFCPair)
+@admin.register(Card)
+class AdminCard(admin.ModelAdmin):
+    list_display = ("name", "source", "dpi", "date")
+
+
+@admin.register(Cardback)
+class AdminCardback(admin.ModelAdmin):
+    list_display = ("name", "source", "dpi", "date")
+
+
+@admin.register(Token)
+class AdminToken(admin.ModelAdmin):
+    list_display = ("name", "source", "dpi", "date")
+
+
+@admin.register(DFCPair)
+class AdminDFCPair(admin.ModelAdmin):
+    list_display = ("front", "back")
+
+
+@admin.register(Source)
+class AdminSource(admin.ModelAdmin):
+    list_display = ("id", "drive_id", "contribution", "description")
+
+    def contribution(self, obj):
+        qty_all, qty_cards, qty_cardbacks, qty_tokens, avgdpi = obj.count()
+
+        return "{} images - {} cards, {} cardbacks, and {} tokens @ {} DPI on average".format(
+            qty_all, qty_cards, qty_cardbacks, qty_tokens, avgdpi
+        )
