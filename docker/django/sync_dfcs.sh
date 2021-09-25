ce25ce1649840daccde278166e43457194a0a155
Florian Klemme <mail@florianklemme.de>
2021-09-25 15:30:07 +0200

    Improved comments and verbosity of elasticsearch.

--- docker/django/sync_dfcs.sh
+++ docker/django/sync_dfcs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Called by cron to update double-faced cards
+
 cd /MPCAutofill/MPCAutofill
 python3 manage.py update_dfcs
--- docker/django/update_database.sh
+++ docker/django/update_database.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Called by cron to scan drives and update the database
+
 cd /MPCAutofill/MPCAutofill
 python3 manage.py update_database
