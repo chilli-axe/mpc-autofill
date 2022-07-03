a8c3efa932e5896a118364608d8151e1e630f405
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2022-07-04 08:04:05 +1000

    forgot to install pre-commit hook on this machine

--- MPCAutofill/cardpicker/migrations/0019_alter_source_drive_link.py
+++ MPCAutofill/cardpicker/migrations/0019_alter_source_drive_link.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cardpicker', '0018_auto_20210922_1827'),
+        ("cardpicker", "0018_auto_20210922_1827"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='source',
-            name='drive_link',
+            model_name="source",
+            name="drive_link",
             field=models.CharField(blank=True, max_length=200, null=True),
         ),
     ]
