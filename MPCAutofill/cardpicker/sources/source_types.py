5a2eacf58a1e5a4f3b13be965908c551cc6001a8
Nicholas de Paola <ndepaola97@gmail.com>
2022-08-06 09:50:08 +1000

    docstrings!

--- MPCAutofill/cardpicker/sources/source_types.py
+++ MPCAutofill/cardpicker/sources/source_types.py
@@ -20,30 +20,63 @@ if TYPE_CHECKING:
 class SourceType:
     @staticmethod
     def get_identifier() -> "SourceTypeChoices":
+        """
+        Map this source type to the `SourceTypeChoices` enum (the `Source` table has a field for `SourceTypeChoices`).
+        """
+
         raise NotImplementedError
 
     @staticmethod
     def get_download_link(identifier: str) -> Optional[str]:
+        """
+        Return the download link for a Card, Cardback, or Token object identified by `identifier`.
+        Return None if providing a download link isn't possible or doesn't make sense.
+        """
+
         raise NotImplementedError
 
     @staticmethod
-    def get_small_thumbnail_url(identifier: str) -> Optional[str]:
+    def get_small_thumbnail_url(identifier: str) -> str:
+        """
+        Return the small thumbnail URL for a Card, Cardback, or Token object identified by `identifier`.
+        This URL will be used when displaying the image in the frontend grid views.
+        """
+
         raise NotImplementedError
 
     @staticmethod
-    def get_medium_thumbnail_url(identifier: str) -> Optional[str]:
+    def get_medium_thumbnail_url(identifier: str) -> str:
+        """
+        Return the medium thumbnail URL for a Card, Cardback, or Token object identified by `identifier`.
+        This URL will be used when displaying the image in the frontend detailed view modal.
+        """
+
         raise NotImplementedError
 
     @staticmethod
     def get_all_folders(sources: list["Source"]) -> dict[str, Optional[Folder]]:
+        """
+        Given the list of `Source` objects which are of this source type, create and return a dictionary
+        which maps each source's key to a `Folder` object representing the root folder of that source
+        (or None if the source is invalid for whatever reason).
+        This will probably involve API calls.
+        """
+
         raise NotImplementedError
 
     @staticmethod
     def get_all_folders_inside_folder(folder: Folder) -> list[Folder]:
+        """
+        Return a list of folders inside `folder`. This will probably involve API calls.
+        """
+
         raise NotImplementedError
 
     @staticmethod
     def get_all_images_inside_folder(folder: Folder) -> list[Image]:
+        """
+        Return a list of images inside `folder`. This will probably involve API calls.
+        """
         raise NotImplementedError
 
 
@@ -57,11 +90,11 @@ class GoogleDrive(SourceType):
         return f"https://drive.google.com/uc?id={identifier}&export=download"
 
     @staticmethod
-    def get_small_thumbnail_url(identifier: str) -> Optional[str]:
+    def get_small_thumbnail_url(identifier: str) -> str:
         return f"https://drive.google.com/thumbnail?sz=w400-h400&id={identifier}"
 
     @staticmethod
-    def get_medium_thumbnail_url(identifier: str) -> Optional[str]:
+    def get_medium_thumbnail_url(identifier: str) -> str:
         return f"https://drive.google.com/thumbnail?sz=w800-h800&id={identifier}"
 
     @staticmethod
@@ -104,6 +137,7 @@ class GoogleDrive(SourceType):
         page_token = None
         images = []
         while True:
+            # continue to query the gdrive API until all pages have been read
             results = execute_google_drive_api_call(
                 service.files().list(
                     q="(mimeType contains 'image/png' or "
