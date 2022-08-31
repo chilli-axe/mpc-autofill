5021a57ebe52ff2c48826527e8c0b59d204df6b8
Nicholas de Paola <nicholas.depaola@polymathian.com>
2022-08-31 23:35:05 +1000

    test xml generation

--- MPCAutofill/cardpicker/tests/test_frontend.py
+++ MPCAutofill/cardpicker/tests/test_frontend.py
@@ -1,3 +1,6 @@
+import os
+import re
+import shutil
 import time
 
 import pytest
@@ -16,22 +19,28 @@ from selenium.webdriver.support.expected_conditions import (
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+FILE_PATH = os.path.abspath(os.path.dirname(__file__))
+DOWNLOAD_FOLDER = os.path.join(FILE_PATH, "downloads")
+
 
 @pytest.fixture()
 def chrome_driver() -> Chrome:
     options = Options()
     options.add_argument("--headless")
+    options.add_experimental_option("prefs", {"download.default_directory": DOWNLOAD_FOLDER})
     driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.set_window_size(1440, 900)
     driver.implicitly_wait(0)
+    os.mkdir(DOWNLOAD_FOLDER)
     yield driver
     driver.quit()
+    shutil.rmtree(DOWNLOAD_FOLDER, ignore_errors=True)
 
 
 class TestFrontend:
     @pytest.fixture(autouse=True)
     def enable_db_access_and_django_debug(self, db, settings):
-        # only within the scope of this class
+        # only autouse within the scope of this class
         settings.DEBUG = True
 
     @pytest.fixture()
@@ -48,7 +57,7 @@ class TestFrontend:
         )
         call_command("update_database")
 
-    def test_basic_search(self, elasticsearch, chrome_driver, live_server):
+    def test_basic_search_and_xml_generation(self, elasticsearch, chrome_driver, live_server):
         chrome_driver.get(live_server.url)
         text_area = chrome_driver.find_element(By.ID, value="id_card_list")
         text_area.send_keys("4 brainstorm\n3 island")
@@ -57,7 +66,7 @@ class TestFrontend:
         load_modal = chrome_driver.find_element(By.ID, value="loadModal")
         WebDriverWait(chrome_driver, 1).until(visibility_of(load_modal))
         WebDriverWait(chrome_driver, 1).until(invisibility_of_element(load_modal))
-        time.sleep(1)
+        time.sleep(2)
 
         assert chrome_driver.find_element(By.ID, value="order_qty").text == "7"
         assert chrome_driver.find_element(By.ID, value="order_bracket").text == "18"
@@ -104,6 +113,41 @@ class TestFrontend:
         assert chrome_driver.find_element(By.ID, value="slot6-front-mpccard-counter").text == "1/1"
         assert "Example Cards" in chrome_driver.find_element(By.ID, value="slot6-front-mpccard-source").text
 
+        chrome_driver.find_element(By.ID, value="btn_generate_xml").click()
+        time.sleep(1)
+        with open(os.path.join(DOWNLOAD_FOLDER, "cards.xml"), "r") as f:
+            assert re.sub(r"[\n\t\s]*", "", str(f.read())) == re.sub(
+                r"[\n\t\s]*",
+                "",
+                (
+                    """
+                <order>
+                    <details>
+                        <quantity>7</quantity>
+                        <bracket>18</bracket>
+                        <stock>(S30) Standard Smooth</stock>
+                        <foil>false</foil>
+                    </details>
+                    <fronts>
+                        <card>
+                            <id>1MapGHeusRE1RD-VKPzDSeABudGXF_4Uh</id>
+                            <slots>1,2,0,3</slots>
+                            <name>Brainstorm.png</name>
+                            <query>brainstorm</query>
+                        </card>
+                        <card>
+                            <id>19aEsBrSKeKcrBBrcCHVSU80J_ViEVhib</id>
+                            <slots>4,5,6</slots>
+                            <name>Island.png</name>
+                            <query>island</query>
+                        </card>
+                    </fronts>
+                    <cardback>11CpSsZ4KuDTvKco5HAa8qcfLgWxV21CP</cardback>
+                </order>
+                """
+                ),
+            )
+
     def test_toggle_faces(self, elasticsearch, chrome_driver, live_server):
         chrome_driver.get(live_server.url)
         text_area = chrome_driver.find_element(By.ID, value="id_card_list")
@@ -113,7 +157,7 @@ class TestFrontend:
         load_modal = chrome_driver.find_element(By.ID, value="loadModal")
         WebDriverWait(chrome_driver, 1).until(visibility_of(load_modal))
         WebDriverWait(chrome_driver, 1).until(invisibility_of_element(load_modal))
-        time.sleep(1)
+        time.sleep(2)
 
         assert chrome_driver.find_element(By.ID, value="slot0-front").is_displayed() is True
         assert chrome_driver.find_element(By.ID, value="slot0-back").is_displayed() is False
