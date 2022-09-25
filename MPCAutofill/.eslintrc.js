6beb8b241a569beea8296dc6b7cc75826f2f07fc
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2022-09-25 14:28:57 +1000

    install es lint pre-commit hook

---
+++ MPCAutofill/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["standard", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
