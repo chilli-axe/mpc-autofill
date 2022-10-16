896280240ea2c32fcd304130f9798e94b38b6e6c
AurelienBou <aurelienbourgeois@orange.fr>
2022-10-16 19:18:16 +0200

    Run black for new class

--- MPCAutofill/cardpicker/tags.py
+++ MPCAutofill/cardpicker/tags.py
@@ -3,8 +3,9 @@ Tags for cards.
 """
 from __future__ import annotations
 
-from aenum import MultiValueEnum
 import re
+
+from aenum import MultiValueEnum
 
 
 class Tag(str, MultiValueEnum):
@@ -16,7 +17,7 @@ class Tag(str, MultiValueEnum):
     @staticmethod
     def list_from_card_name(card_name: str) -> list[Tag]:
         tag_set = set()
-        pattern = r"\(([^\(\)]+)\)|\[([^\[\]]+)\]" # Get content of () and []
+        pattern = r"\(([^\(\)]+)\)|\[([^\[\]]+)\]"  # Get content of () and []
         tag_parts = re.findall(pattern, card_name)
         cleaned_parts = list(map(lambda x: x[0] if len(x[0]) != 0 else x[1], tag_parts))
         for tag_part in cleaned_parts:
@@ -25,6 +26,6 @@ class Tag(str, MultiValueEnum):
                 try:
                     tag_set.add(Tag(raw_tag.strip().upper()))
                 except ValueError:
-                    pass # Unknown tag
+                    pass  # Unknown tag
 
         return list(tag_set)
