8a8211b0a4907bacc972e70d130f3bc08fac7855
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2022-10-23 13:21:30 +1000

    update db image size command

--- MPCAutofill/cardpicker/management/commands/db_image_size.py
+++ MPCAutofill/cardpicker/management/commands/db_image_size.py
@@ -3,7 +3,7 @@ from typing import Any
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
 
-from cardpicker.models import Card
+from cardpicker.models import Card, CardTypes
 
 
 class Command(BaseCommand):
@@ -11,5 +11,18 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **kwargs: dict[str, Any]) -> None:
         # store as GB
-        card_size = round(Card.objects.aggregate(Sum("size"))["size__sum"] / 1_000_000_000)
-        print(f"Total size: {(card_size)/1000} TB")
+        card_size = round(
+            Card.objects.filter(card_type=CardTypes.CARD).aggregate(Sum("size"))["size__sum"] / 1_000_000_000
+        )
+        cardback_size = round(
+            Card.objects.filter(card_type=CardTypes.CARDBACK).aggregate(Sum("size"))["size__sum"] / 1_000_000_000
+        )
+        token_size = round(
+            Card.objects.filter(card_type=CardTypes.TOKEN).aggregate(Sum("size"))["size__sum"] / 1_000_000_000
+        )
+        print(
+            f"Total size: {(card_size + cardback_size + token_size)/1000} TB - "
+            f"cardbacks: {card_size} GB, "
+            f"tokens: {cardback_size} GB, "
+            f"tokens: {token_size} GB"
+        )
