674865d77cc4abd35e79cb95f53b5465b1d28388
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-01-18 22:49:36 +1000

    configure typescript-eslint in pre-commit

--- MPCAutofill/.eslintrc.js
+++ MPCAutofill/.eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     es2021: true,
     jquery: true,
   },
-  extends: ["standard", "prettier"],
+  extends: ["standard", "prettier", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
@@ -15,4 +17,5 @@ module.exports = {
     camelcase: "off", // TODO: remove this and convert things to camelcase
     "no-global-assign": "off",
   },
+  root: true,
 };
