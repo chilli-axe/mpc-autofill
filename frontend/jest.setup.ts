ac43127c4bfeb2a0c2c1f30c8a69cc4008250b0e
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-04-09 00:31:29 +1000

    lmfao what happened here to break my google analytics tests

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,6 +1,17 @@
 // The below can be used in a Jest global setup file or similar for your testing set-up
 import { loadEnvConfig } from "@next/env";
 
+// retrieved from https://stackoverflow.com/a/68539103/13021511
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
 export default async () => {
   const projectDir = process.cwd();
   loadEnvConfig(projectDir);
