86670e79f954e40fbc3ceaf729926ee769193598
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-04-10 17:45:01 +1000

    seems that we have to manually specify the font in next.js as well
    https://nextjs.org/docs/basic-features/font-optimization

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "./../styles/styles.scss";
 import "./../styles/custom.css";
 import { AppProps } from "next/app";
+import { Lato } from "next/font/google";
+
+// unfortunately, for static deployment, we need to specify our Bootswatch theme font here again :/
+// google font config retrieved from https://github.com/thomaspark/bootswatch/blob/v5/dist/superhero/_bootswatch.scss
+const lato = Lato({
+  weight: ["300", "400", "700"],
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
