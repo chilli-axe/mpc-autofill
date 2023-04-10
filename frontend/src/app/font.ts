51f399cda5167031a0dcc21760fda222fb88f3ad
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-04-11 00:37:28 +1000

    hopefully upgrading next.js to latest fixes this?

--- frontend/src/app/font.ts
+++
@@ -1,9 +0,0 @@
-// unfortunately, for static deployment, we need to specify our Bootswatch theme font here again :/
-// google font config retrieved from https://github.com/thomaspark/bootswatch/blob/v5/dist/superhero/_bootswatch.scss
-import { Lato } from "next/font/google";
-
-export const lato = Lato({
-  weight: ["300", "400", "700"],
-  subsets: ["latin", "latin-ext"],
-  display: "swap",
-});
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import "./../styles/styles.scss";
 import "./../styles/custom.css";
 import { AppProps } from "next/app";
-import { lato } from "@/app/font";
+
+// unfortunately, for static deployment, we need to specify our Bootswatch theme font here again :/
+// google font config retrieved from https://github.com/thomaspark/bootswatch/blob/v5/dist/superhero/_bootswatch.scss
+import { Lato } from "next/font/google";
+
+export const lato = Lato({
+  weight: ["300", "400", "700"],
+  subsets: ["latin", "latin-ext"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
