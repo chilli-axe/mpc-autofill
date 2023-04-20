a41e6776a965cae9901ac44e84b2903b2e08f9cb
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-04-20 23:05:34 +1000

    minor cleanup of last commit

--- frontend/src/features/card/gridSelector.tsx
+++ frontend/src/features/card/gridSelector.tsx
@@ -41,9 +41,8 @@ export function GridSelector(props: GridSelectorProps) {
     });
   };
 
-  // aggregate cards by source.
   // TODO: we should probably consider returning the data from the backend like this.
-  const mymen2 = props.imageIdentifiers.reduce(
+  const cardIdentifiersAndOptionNumbersBySource = props.imageIdentifiers.reduce(
     (
       accumulator: { [sourceName: string]: Array<[string, number]> },
       value,
@@ -79,45 +78,55 @@ export function GridSelector(props: GridSelectorProps) {
         <Modal.Title>Select Version</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {Object.entries(mymen2).map(([key, value], outerIndex) => (
-          <>
-            <div
-              className="d-flex justify-content-between"
-              onClick={() => toggleSourceCollapsed(key)}
-            >
-              <h4 className="orpheus">
-                <i>{key}</i>
-              </h4>
-              <i
-                className={
-                  collapsedState[key] ?? true
-                    ? "bi bi-chevron-down"
-                    : "bi bi-chevron-left"
-                }
-              ></i>
-            </div>
-            {(collapsedState[key] ?? true) && (
-              <>
-                <hr />
-                <Row className="g-0" xxl={4} xl={4} lg={3} md={2} sm={2} xs={2}>
-                  {value.map(([identifier, innerIndex]) => (
-                    <Card
-                      imageIdentifier={identifier}
-                      cardHeaderTitle={`Option ${innerIndex + 1}`}
-                      cardOnClick={() => {
-                        props.onClick(identifier);
-                        props.handleClose();
-                      }}
-                      key={`gridSelector-${identifier}`}
-                      noResultsFound={false}
-                    />
-                  ))}
-                </Row>
-              </>
-            )}
-            {outerIndex != Object.keys(mymen2).length - 1 && <hr />}
-          </>
-        ))}
+        {Object.entries(cardIdentifiersAndOptionNumbersBySource).map(
+          ([key, value]) => (
+            <>
+              <div
+                className="d-flex justify-content-between"
+                onClick={() => toggleSourceCollapsed(key)}
+              >
+                <h4 className="orpheus">
+                  <i>{key}</i>
+                </h4>
+                <i
+                  className={
+                    collapsedState[key] ?? true
+                      ? "bi bi-chevron-down"
+                      : "bi bi-chevron-left"
+                  }
+                ></i>
+              </div>
+              {(collapsedState[key] ?? true) && (
+                <>
+                  <hr />
+                  <Row
+                    className="g-0"
+                    xxl={4}
+                    xl={4}
+                    lg={3}
+                    md={2}
+                    sm={2}
+                    xs={2}
+                  >
+                    {value.map(([identifier, index]) => (
+                      <Card
+                        imageIdentifier={identifier}
+                        cardHeaderTitle={`Option ${index + 1}`}
+                        cardOnClick={() => {
+                          props.onClick(identifier);
+                          props.handleClose();
+                        }}
+                        key={`gridSelector-${identifier}`}
+                        noResultsFound={false}
+                      />
+                    ))}
+                  </Row>
+                </>
+              )}
+              <hr />
+            </>
+          )
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={props.handleClose}>
