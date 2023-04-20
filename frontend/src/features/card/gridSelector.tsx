37c2f11df0eb52900578ecbe954265d746d11d7b
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-04-20 22:54:26 +1000

    super rough first draft of faceting cards by source in grid selector

--- frontend/src/features/card/gridSelector.tsx
+++ frontend/src/features/card/gridSelector.tsx
@@ -8,7 +8,10 @@ import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
 import { Card } from "./card";
 import Button from "react-bootstrap/Button";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/store";
+import { CardDocument, CardDocuments } from "@/common/types";
 
 interface GridSelectorProps {
   testId: string;
@@ -24,6 +27,47 @@ interface GridSelectorProps {
 }
 
 export function GridSelector(props: GridSelectorProps) {
+  const [collapsedState, setCollapsedState] = useState<{
+    [source: string]: boolean;
+  }>({});
+  const cardDocuments: CardDocuments = useSelector(
+    (state: RootState) => state.cardDocuments.cardDocuments
+  );
+
+  const toggleSourceCollapsed = (source: string) => {
+    setCollapsedState({
+      ...collapsedState,
+      [source]: !(collapsedState[source] ?? true),
+    });
+  };
+
+  // aggregate cards by source.
+  // TODO: we should probably consider returning the data from the backend like this.
+  const mymen2 = props.imageIdentifiers.reduce(
+    (
+      accumulator: { [sourceName: string]: Array<[string, number]> },
+      value,
+      currentIndex
+    ) => {
+      const cardDocument: CardDocument | null = cardDocuments[value];
+      if (cardDocument != null) {
+        if (
+          !Object.prototype.hasOwnProperty.call(
+            accumulator,
+            cardDocument.source
+          )
+        ) {
+          accumulator[cardDocument.source] = [];
+        }
+        accumulator[cardDocument.source].push([value, currentIndex]);
+      }
+      return accumulator;
+    },
+    {}
+  );
+
+  // TODO: paginate or lazy-load these cards. this is quite slow when you have hundreds of images.
+
   return (
     <Modal
       show={props.show}
@@ -35,20 +79,45 @@ export function GridSelector(props: GridSelectorProps) {
         <Modal.Title>Select Version</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Row className="g-0" xxl={4} xl={4} lg={3} md={2} sm={2} xs={2}>
-          {props.imageIdentifiers.map((identifier, index) => (
-            <Card // TODO: paginate or lazy-load these
-              imageIdentifier={identifier}
-              cardHeaderTitle={`Option ${index + 1}`}
-              cardOnClick={() => {
-                props.onClick(identifier);
-                props.handleClose();
-              }}
-              key={`gridSelector-${identifier}`}
-              noResultsFound={false}
-            />
-          ))}
-        </Row>
+        {Object.entries(mymen2).map(([key, value], outerIndex) => (
+          <>
+            <div
+              className="d-flex justify-content-between"
+              onClick={() => toggleSourceCollapsed(key)}
+            >
+              <h4 className="orpheus">
+                <i>{key}</i>
+              </h4>
+              <i
+                className={
+                  collapsedState[key] ?? true
+                    ? "bi bi-chevron-down"
+                    : "bi bi-chevron-left"
+                }
+              ></i>
+            </div>
+            {(collapsedState[key] ?? true) && (
+              <>
+                <hr />
+                <Row className="g-0" xxl={4} xl={4} lg={3} md={2} sm={2} xs={2}>
+                  {value.map(([identifier, innerIndex]) => (
+                    <Card
+                      imageIdentifier={identifier}
+                      cardHeaderTitle={`Option ${innerIndex + 1}`}
+                      cardOnClick={() => {
+                        props.onClick(identifier);
+                        props.handleClose();
+                      }}
+                      key={`gridSelector-${identifier}`}
+                      noResultsFound={false}
+                    />
+                  ))}
+                </Row>
+              </>
+            )}
+            {outerIndex != Object.keys(mymen2).length - 1 && <hr />}
+          </>
+        ))}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={props.handleClose}>
