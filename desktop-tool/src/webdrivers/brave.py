001ad675811d4381faca84df512303a4d59a2007
Nicholas de Paola <ndepaola97@gmail.com>
2023-05-07 16:08:35 +1000

    resolve issue where brave isn't correctly picked up on linux
    webdriver manager wasn't respecting the `chrome_type` of brave
    and trying to run the tool with chrome instead.
    
    it turns out that brave support was added to webdriver-manager in 3.5.4
    https://github.com/SergeyPirogov/webdriver_manager/releases/tag/v3.5.4
    
    upgrading to the newest webdriver manager version here.
    
    brave support may have regressed when i pinned an older version of
    webdriver-manager because of broken logging. not completely sure :/
    
    should resolve https://github.com/chilli-axe/mpc-autofill/issues/142
    
    will need to test on macos and windows before including in a release.

--- desktop-tool/src/webdrivers/brave.py
+++ desktop-tool/src/webdrivers/brave.py
@@ -1,7 +1,10 @@
+import os
+
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager, ChromeType
+from webdriver_manager.core.utils import OSType, os_name
 
 
 def get_brave_driver(headless: bool = False) -> Chrome:
@@ -12,6 +15,17 @@ def get_brave_driver(headless: bool = False) -> Chrome:
         options.add_argument("--headless")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option("detach", True)
+
+    # the binary location for brave must be manually specified
+    # the below code block is borrowed from webdriver-manager tests:
+    # https://github.com/SergeyPirogov/webdriver_manager/blob/master/tests/test_brave_driver.py
+    binary_location = {
+        OSType.LINUX: "/usr/bin/brave-browser",
+        OSType.MAC: "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser",
+        OSType.WIN: f"{os.getenv('LOCALAPPDATA')}\\BraveSoftware\\Brave-Browser\\Application\\brave.exe",
+    }[os_name()]
+    options.binary_location = binary_location
+
     driver = Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options)  # type: ignore
     driver.set_network_conditions(offline=False, latency=5, throughput=5 * 125000)
     return driver
