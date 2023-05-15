502e253b914f344bae0a6b7bb3b6b76572e9cc45
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-05-15 10:10:33 +1000

    more csv testing

--- frontend/src/features/import/importCSV.test.tsx
+++ frontend/src/features/import/importCSV.test.tsx
@@ -5,6 +5,10 @@ import { Back, Front } from "@/common/constants";
 import {
   cardDocument1,
   cardDocument2,
+  cardDocument3,
+  cardDocument4,
+  cardDocument5,
+  cardDocument6,
   localBackend,
 } from "@/common/test-constants";
 import {
@@ -17,10 +21,15 @@ import {
 } from "@/common/test-utils";
 import {
   cardbacksTwoOtherResults,
+  cardDocumentsSixResults,
   cardDocumentsThreeResults,
   defaultHandlers,
+  searchResultsFourResults,
   searchResultsOneResult,
+  searchResultsSixResults,
+  searchResultsThreeResults,
   sourceDocumentsOneResult,
+  sourceDocumentsThreeResults,
 } from "@/mocks/handlers";
 import { server } from "@/mocks/server";
 
@@ -55,11 +64,122 @@ test("importing one card by CSV into an empty project", async () => {
   renderWithProviders(<App />, { preloadedState });
 
   // import a card
-  await importCSV("Quantity,Front\n,my search query");
+  await importCSV(
+    `Quantity,Front
+    ,my search query`
+  );
 
   // a card slot should have been created
   await expectCardSlotToExist(1);
   await expectCardGridSlotState(1, Front, cardDocument1.name, 1, 1);
   await expectCardGridSlotState(1, Back, cardDocument2.name, 1, 2);
+  await expectCardbackSlotState(cardDocument2.name, 1, 2);
+});
+
+test("importing multiple instances of one card by CSV into an empty project", async () => {
+  server.use(
+    cardDocumentsThreeResults,
+    cardbacksTwoOtherResults,
+    sourceDocumentsOneResult,
+    searchResultsOneResult,
+    ...defaultHandlers
+  );
+  renderWithProviders(<App />, { preloadedState });
+
+  // import two instances of a card
+  await importCSV(
+    `Quantity,Front
+    2,my search query`
+  );
+
+  // two card slots should have been created
+  await expectCardSlotToExist(1);
+  await expectCardGridSlotState(1, Front, cardDocument1.name, 1, 1);
+  await expectCardGridSlotState(1, Back, cardDocument2.name, 1, 2);
+  await expectCardSlotToExist(2);
+  await expectCardGridSlotState(2, Front, cardDocument1.name, 1, 1);
+  await expectCardGridSlotState(2, Back, cardDocument2.name, 1, 2);
+  await expectCardbackSlotState(cardDocument2.name, 1, 2);
+});
+
+test("importing one specific card version by CSV into an empty project", async () => {
+  server.use(
+    cardDocumentsThreeResults,
+    cardbacksTwoOtherResults,
+    sourceDocumentsOneResult,
+    searchResultsThreeResults,
+    ...defaultHandlers
+  );
+  renderWithProviders(<App />, { preloadedState });
+
+  // import a card
+  await importCSV(
+    `Quantity,Front,Front ID
+    ,my search query,${cardDocument3.identifier}`
+  );
+
+  // a card slot should have been created
+  await expectCardSlotToExist(1);
+  await expectCardGridSlotState(1, Front, cardDocument3.name, 3, 3);
+  await expectCardGridSlotState(1, Back, cardDocument2.name, 1, 2);
+  await expectCardbackSlotState(cardDocument2.name, 1, 2);
+});
+
+test("importing one card of each type into an empty project", async () => {
+  server.use(
+    cardDocumentsSixResults,
+    cardbacksTwoOtherResults,
+    sourceDocumentsThreeResults,
+    searchResultsSixResults,
+    ...defaultHandlers
+  );
+  renderWithProviders(<App />, { preloadedState });
+
+  // import one card of each type
+  await importCSV(
+    `Quantity,Front
+    ,query 1\n,t:query 6\n,b:query 5`
+  );
+
+  // three card slots should have been created
+  await expectCardSlotToExist(1);
+  await expectCardGridSlotState(1, Front, cardDocument1.name, 1, 1);
+  await expectCardGridSlotState(1, Back, cardDocument2.name, 1, 2);
+  await expectCardSlotToExist(2);
+  await expectCardGridSlotState(2, Front, cardDocument6.name, 1, 1);
+  await expectCardGridSlotState(2, Back, cardDocument2.name, 1, 2);
+  await expectCardSlotToExist(3);
+  await expectCardGridSlotState(3, Front, cardDocument5.name, 1, 1);
+  await expectCardGridSlotState(3, Back, cardDocument2.name, 1, 2);
+  await expectCardbackSlotState(cardDocument2.name, 1, 2);
+});
+
+test("importing a more complex CSV into an empty project", async () => {
+  server.use(
+    cardDocumentsSixResults,
+    cardbacksTwoOtherResults,
+    sourceDocumentsOneResult,
+    searchResultsFourResults,
+    ...defaultHandlers
+  );
+  renderWithProviders(<App />, { preloadedState });
+
+  // import a few cards
+  await importCSV(
+    `Quantity,Front,Front ID,Back,Back ID
+    2,my search query,${cardDocument3.identifier},my search query,${cardDocument4.identifier}
+    ,my search query`
+  );
+
+  // three card slots should have been created
+  await expectCardSlotToExist(1);
+  await expectCardGridSlotState(1, Front, cardDocument3.name, 3, 4);
+  await expectCardGridSlotState(1, Back, cardDocument4.name, 4, 4);
+  await expectCardSlotToExist(2);
+  await expectCardGridSlotState(2, Front, cardDocument3.name, 3, 4);
+  await expectCardGridSlotState(2, Back, cardDocument4.name, 4, 4);
+  await expectCardSlotToExist(3);
+  await expectCardGridSlotState(3, Front, cardDocument1.name, 1, 4);
+  await expectCardGridSlotState(3, Back, cardDocument2.name, 1, 2);
   await expectCardbackSlotState(cardDocument2.name, 1, 2);
 });
