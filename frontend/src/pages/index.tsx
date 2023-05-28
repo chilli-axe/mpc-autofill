2b5034069d8fdbb205a91de3a0774376129a7d0c
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-05-28 12:58:35 +1000

    try to tidy up a bit

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import React, { useCallback, useState } from "react";
 import Container from "react-bootstrap/Container";
 import { useSelector } from "react-redux";
+import styled, { StyledComponent } from "styled-components";
 
 import { useGetSampleCardsQuery } from "@/app/api";
 import { ProjectName } from "@/common/constants";
+import { CardDocument } from "@/common/types";
 import { selectBackendURL } from "@/features/backend/backendSlice";
 import {
   MemoizedCardImage,
@@ -15,6 +17,70 @@ import { MemoizedCardDetailedView } from "@/features/card/cardDetailedView";
 import Footer from "@/features/ui/footer";
 import Layout from "@/features/ui/layout";
 
+const DynamicLogoContainer = styled(Container)`
+  position: relative;
+  height: 620px;
+  width: 620px;
+  background: linear-gradient(#4692f0, #183251);
+  border-radius: 300px;
+  outline: solid 4px black;
+`;
+
+const DynamicLogoLabel = styled.p`
+  position: absolute;
+  font-weight: bold;
+  font-style: italic;
+  top: 80px;
+  left: 50%;
+  z-index: 10;
+  text-align: center;
+  transform: translate(-50%, 0px);
+  font-size: 3.2em;
+  text-shadow: 0 4px 15px #000000;
+`;
+
+const DynamicLogoArrow = styled(Image)`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0px);
+`;
+
+const ImageTransformWrapperBase = styled.div`
+  position: absolute;
+  height: 200px;
+  width: 200px;
+  left: 210px;
+  top: 185px;
+`;
+
+const FirstImageTransformWrapper = styled(ImageTransformWrapperBase)`
+  transform: translate(-200px, -20px);
+  rotate: -40deg;
+  z-index: 0;
+`;
+const SecondImageTransformWrapper = styled(ImageTransformWrapperBase)`
+  transform: translate(-100px, 15px);
+  rotate: -20deg;
+  z-index: 1;
+`;
+
+const ThirdImageTransformWrapper = styled(ImageTransformWrapperBase)`
+  transform: translate(0px, 15px);
+  z-index: 2;
+`;
+
+const FourthImageTransformWrapper = styled(ImageTransformWrapperBase)`
+  transform: translate(100px, 15px);
+  rotate: 20deg;
+  z-index: 3;
+`;
+
+const FifthImageTransformWrapper = styled(ImageTransformWrapperBase)`
+  transform: translate(200px, -20px);
+  rotate: 40deg;
+  z-index: 4;
+`;
+
 function DynamicLogo() {
   // 57f653
   // TODO: set up custom hooks for using queries in this way (i.e. not querying until backend URL is specified)
@@ -22,209 +88,66 @@ function DynamicLogo() {
   const sampleCardsQuery = useGetSampleCardsQuery(undefined, {
     skip: backendURL == null,
   });
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<CardDocument | null>(null);
   const [showDetailedView, setShowDetailedView] = useState<boolean>(false);
   const handleCloseDetailedView = () => setShowDetailedView(false);
-  const handleShowDetailedView = useCallback((img: string) => {
-    setSelectedImage(img);
+  const handleShowDetailedView = useCallback((card: CardDocument) => {
+    setSelectedImage(card);
     setShowDetailedView(true);
   }, []);
 
-  const documentsByIdentifier = sampleCardsQuery.isSuccess
-    ? {
-        [sampleCardsQuery.data["TOKEN"][0].identifier]:
-          sampleCardsQuery.data["TOKEN"][0],
-        [sampleCardsQuery.data["CARD"][0].identifier]:
-          sampleCardsQuery.data["CARD"][0],
-        [sampleCardsQuery.data["CARD"][1].identifier]:
-          sampleCardsQuery.data["CARD"][1],
-        [sampleCardsQuery.data["CARD"][2].identifier]:
-          sampleCardsQuery.data["CARD"][2],
-        [sampleCardsQuery.data["CARD"][3].identifier]:
-          sampleCardsQuery.data["CARD"][3],
-      }
-    : {};
+  // TODO: handle the no data case with a default image that reads "your design here" or something
+  const displayCards: Array<
+    [CardDocument, StyledComponent<"div", any, {}, never>]
+  > = sampleCardsQuery.isSuccess
+    ? [
+        [sampleCardsQuery.data["TOKEN"][0], FirstImageTransformWrapper],
+        [sampleCardsQuery.data["CARD"][0], SecondImageTransformWrapper],
+        [sampleCardsQuery.data["CARD"][1], ThirdImageTransformWrapper],
+        [sampleCardsQuery.data["CARD"][2], FourthImageTransformWrapper],
+        [sampleCardsQuery.data["CARD"][3], FifthImageTransformWrapper],
+      ]
+    : [];
 
   return (
     <>
       {sampleCardsQuery.isSuccess ? (
-        <Container
-          className="shadow-lg"
-          style={{
-            position: "relative",
-            height: 620 + "px",
-            width: 620 + "px",
-            background: "linear-gradient(#4692f0, #183251)",
-            borderRadius: 300 + "px",
-            outline: "solid",
-            outlineWidth: 4 + "px",
-            outlineColor: "black",
-          }}
-        >
-          <p
-            className="orpheus"
-            style={{
-              position: "absolute",
-              fontWeight: "bold",
-              fontStyle: "italic",
-              top: 80 + "px",
-              left: 50 + "%",
-              zIndex: 10,
-              textAlign: "center",
-              transform: "translate(-50%, 0px)",
-              fontSize: 3.2 + "em",
-              textShadow: "0px 4px 15px #000000",
-            }}
-          >
-            MPC Autofill
-          </p>
-          <Image
+        <DynamicLogoContainer className="shadow-lg">
+          <DynamicLogoLabel className="orpheus">MPC Autofill</DynamicLogoLabel>
+          <DynamicLogoArrow
             src="/arrow.svg"
             alt="logo-arrow"
             width={250}
             height={250}
             quality={100}
-            style={{
-              position: "absolute",
-              left: 50 + "%",
-              transform: "translate(-50%, 0px)",
-            }}
           />
 
-          <div
-            style={{
-              position: "absolute",
-              height: 200 + "px",
-              width: 200 + "px",
-              left: 210 + "px",
-              top: 185 + "px",
-              transform: "translate(-200px, -20px)",
-              zIndex: 0,
-              rotate: -40 + "deg",
-            }}
-          >
-            <MemoizedCardProportionWrapper small={true}>
-              <MemoizedCardImage
-                cardDocument={sampleCardsQuery.data["TOKEN"][0]}
-                hidden={false}
+          {displayCards.map(([cardDocument, WrapperElement]) => (
+            <WrapperElement key={`${cardDocument.identifier}-outer-wrapper`}>
+              <MemoizedCardProportionWrapper
                 small={true}
-                onClick={() =>
-                  handleShowDetailedView(
-                    sampleCardsQuery.data["TOKEN"][0].identifier
-                  )
-                }
-              />
-            </MemoizedCardProportionWrapper>
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              height: 200 + "px",
-              width: 200 + "px",
-              left: 210 + "px",
-              top: 185 + "px",
-              transform: "translate(-100px, 15px)",
-              zIndex: 1,
-              rotate: -20 + "deg",
-            }}
-          >
-            <MemoizedCardProportionWrapper small={true}>
-              <MemoizedCardImage
-                cardDocument={sampleCardsQuery.data["CARD"][0]}
-                hidden={false}
-                small={true}
-                onClick={() =>
-                  handleShowDetailedView(
-                    sampleCardsQuery.data["CARD"][0].identifier
-                  )
-                }
-              />
-            </MemoizedCardProportionWrapper>
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              height: 200 + "px",
-              width: 200 + "px",
-              left: 210 + "px",
-              top: 185 + "px",
-              transform: "translate(0px, 15px)",
-              zIndex: 2,
-            }}
-          >
-            <MemoizedCardProportionWrapper small={true}>
-              <MemoizedCardImage
-                cardDocument={sampleCardsQuery.data["CARD"][1]}
-                hidden={false}
-                small={true}
-                onClick={() =>
-                  handleShowDetailedView(
-                    sampleCardsQuery.data["CARD"][1].identifier
-                  )
-                }
-              />
-            </MemoizedCardProportionWrapper>
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              height: 200 + "px",
-              width: 200 + "px",
-              left: 210 + "px",
-              top: 185 + "px",
-              transform: "translate(100px, 15px)",
-              zIndex: 3,
-              rotate: 20 + "deg",
-            }}
-          >
-            <MemoizedCardProportionWrapper small={true}>
-              <MemoizedCardImage
-                cardDocument={sampleCardsQuery.data["CARD"][2]}
-                hidden={false}
-                small={true}
-                onClick={() =>
-                  handleShowDetailedView(
-                    sampleCardsQuery.data["CARD"][2].identifier
-                  )
-                }
-              />
-            </MemoizedCardProportionWrapper>
-          </div>
-          <div
-            style={{
-              position: "absolute",
-              height: 200 + "px",
-              width: 200 + "px",
-              left: 210 + "px",
-              top: 185 + "px",
-              transform: "translate(200px, -20px)",
-              zIndex: 4,
-              rotate: 40 + "deg",
-            }}
-          >
-            <MemoizedCardProportionWrapper small={true}>
-              <MemoizedCardImage
-                cardDocument={sampleCardsQuery.data["CARD"][3]}
-                hidden={false}
-                small={true}
-                onClick={() =>
-                  handleShowDetailedView(
-                    sampleCardsQuery.data["CARD"][3].identifier
-                  )
-                }
-              />
-            </MemoizedCardProportionWrapper>
-          </div>
-        </Container>
+                key={`${cardDocument.identifier}-inner-wrapper`}
+              >
+                <MemoizedCardImage
+                  key={`${cardDocument.identifier}-image`}
+                  cardDocument={cardDocument}
+                  hidden={false}
+                  small={true}
+                  onClick={() => handleShowDetailedView(cardDocument)}
+                />
+              </MemoizedCardProportionWrapper>
+            </WrapperElement>
+          ))}
+        </DynamicLogoContainer>
       ) : (
         <div />
       )}
       {selectedImage != null && (
         <MemoizedCardDetailedView
-          imageIdentifier={selectedImage}
+          imageIdentifier={selectedImage.identifier}
           show={showDetailedView}
           handleClose={handleCloseDetailedView}
-          cardDocument={documentsByIdentifier[selectedImage]}
+          cardDocument={selectedImage}
         />
       )}
     </>
