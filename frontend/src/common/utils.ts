deaf7f0abc0c61687a59cd6c94a6a4622057b37e
Nicholas de Paola <ndepaola97@gmail.com>
2023-07-22 21:26:01 +1000

    add an accordion explaining the syntax for card searching

--- frontend/src/common/utils.ts
+++ frontend/src/common/utils.ts
@@ -30,3 +30,7 @@ export function bracket(projectSize: number): number {
   }
   return Brackets[Brackets.length - 1];
 }
+
+export function toTitleCase(word: string): string {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
