fc6da8f8d34115eab418868aed3a412c14f0607f
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-07-29 12:35:26 +1000

    update webdriver args to try to fix CI test failure
    * Message: unknown error: Microsoft Edge failed to start: crashed.
    * (unknown error: DevToolsActivePort file doesn't exist)
    * (The process started from msedge location 
      /opt/hostedtoolcache/msedge/beta/x64/msedge is no longer running,
      so msedgedriver is assuming that msedge has crashed.)

--- desktop-tool/src/webdrivers/brave.py
+++ desktop-tool/src/webdrivers/brave.py
@@ -9,10 +9,11 @@ from webdriver_manager.core.os_manager import OperationSystemManager, OSType
 
 def get_brave_driver(headless: bool = False, binary_location: Optional[str] = None) -> Chrome:
     options = Options()
+    options.add_argument("--no-sandbox")
     options.add_argument("--log-level=3")
     options.add_argument("--disable-dev-shm-usage")
     if headless:
-        options.add_argument("--headless")
+        options.add_argument("headless")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option("detach", True)
 
--- desktop-tool/src/webdrivers/chrome.py
+++ desktop-tool/src/webdrivers/chrome.py
@@ -8,10 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def get_chrome_driver(headless: bool = False, binary_location: Optional[str] = None) -> Chrome:
     options = Options()
+    options.add_argument("--no-sandbox")
     options.add_argument("--log-level=3")
     options.add_argument("--disable-dev-shm-usage")
     if headless:
-        options.add_argument("--headless")
+        options.add_argument("headless")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option("detach", True)
     if binary_location is not None:
