fb1ef03847c07495837cb766ea9ea3488e68e4e0
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2023-07-29 13:41:44 +1000

    manually set the location of chrome webdriver in macos
    motivated by https://github.com/seleniumHQ/selenium/issues/12381

--- desktop-tool/src/webdrivers/chrome.py
+++ desktop-tool/src/webdrivers/chrome.py
@@ -4,6 +4,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import OperationSystemManager, OSType
 
 
 def get_chrome_driver(headless: bool = False, binary_location: Optional[str] = None) -> Chrome:
@@ -17,6 +18,14 @@ def get_chrome_driver(headless: bool = False, binary_location: Optional[str] = N
     options.add_experimental_option("detach", True)
     if binary_location is not None:
         options.binary_location = binary_location
+    else:
+        # here, we specifically hardcode the binary location in macOS to work around this issue:
+        # https://github.com/seleniumHQ/selenium/issues/12381, which is caused by
+        # https://github.com/GoogleChromeLabs/chrome-for-testing/issues/30
+        default_binary_location = {OSType.MAC: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"}.get(
+            OperationSystemManager.get_os_name()
+        )
+        options.binary_location = default_binary_location
     driver = Chrome(service=Service(ChromeDriverManager().install()), options=options)  # type: ignore
     driver.set_network_conditions(offline=False, latency=5, throughput=5 * 125000)
     return driver
