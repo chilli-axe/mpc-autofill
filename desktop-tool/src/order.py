5faa7308cbec8a7bb2691a119af5fea2a60a4333
Nicholas de Paola <ndepaola97@gmail.com>
2024-02-18 23:39:29 +1000

    add functionality for combining & splitting multiple orders
    [wip] a quick sketch of order combining. will flesh this out later.
    bit late here (11:39pm) and i have work tomorrow
    
    continued sketching of order combining & splitting
    parking this again at 11:13pm
    
    continue with order combination
    it seems like it's now customary to note i'm writing this at 10:33pm

--- desktop-tool/src/order.py
+++ desktop-tool/src/order.py
@@ -1,15 +1,17 @@
 import os
 import sys
 from concurrent.futures import ThreadPoolExecutor
+from functools import reduce
 from glob import glob
+from itertools import groupby
 from queue import Queue
 from typing import Optional
 from xml.etree.ElementTree import Element, ParseError
 
 import attr
 import enlighten
-import InquirerPy
 from defusedxml.ElementTree import parse as defused_parse
+from InquirerPy import prompt
 from sanitize_filename import sanitize
 
 from src import constants
@@ -28,7 +30,7 @@ from src.utils import bold, text_to_list, unpack_element
 @attr.s
 class CardImage:
     drive_id: str = attr.ib(default="")
-    slots: list[int] = attr.ib(default=[])
+    slots: list[int] = attr.ib(factory=list)
     name: Optional[str] = attr.ib(default="")
     file_path: Optional[str] = attr.ib(default="")
     query: Optional[str] = attr.ib(default=None)
@@ -154,6 +156,37 @@ class CardImage:
         finally:
             queue.put(self)
             download_bar.update()
+            download_bar.refresh()
+
+    def offset_slots(self, offset: int) -> "CardImage":
+        return CardImage(
+            drive_id=self.drive_id,
+            slots=[slot + offset for slot in self.slots],
+            name=self.name,
+            file_path=self.file_path,
+            query=self.query,
+        )
+
+    def split(self, splits: list[int]) -> list[Optional["CardImage"]]:
+        if not splits:
+            return [self]
+        split_cards: list[Optional["CardImage"]] = [None] * len(splits)
+        splits_with_ends = [0, *splits]
+        for i in range(0, len(splits_with_ends[:-1])):
+            slots_in_split = [
+                slot - splits_with_ends[i]
+                for slot in self.slots
+                if splits_with_ends[i] <= slot < splits_with_ends[i + 1]
+            ]
+            if slots_in_split:
+                split_cards[i] = CardImage(
+                    drive_id=self.drive_id,
+                    slots=slots_in_split,
+                    name=self.name,
+                    file_path=self.file_path,
+                    query=self.query,
+                )
+        return split_cards
 
     # endregion
 
@@ -164,10 +197,19 @@ class CardImageCollection:
     A collection of CardImages for one face of a CardOrder.
     """
 
-    cards: list[CardImage] = attr.ib(default=[])
+    cards: list[CardImage] = attr.ib(factory=list)
     queue: Queue[CardImage] = attr.ib(init=False, default=attr.Factory(Queue))
     num_slots: int = attr.ib(default=0)
     face: constants.Faces = attr.ib(default=constants.Faces.front)
+
+    def combine(self, other: "CardImageCollection") -> "CardImageCollection":
+        assert self.face == other.face
+        # TODO: identify identical cards and merge their slots.
+        return CardImageCollection(
+            cards=[*self.cards, *[card.offset_slots(self.num_slots) for card in other.cards]],
+            num_slots=self.num_slots + other.num_slots,
+            face=self.face,
+        )
 
     # region initialisation
 
@@ -175,7 +217,7 @@ class CardImageCollection:
         return set(range(0, self.num_slots))
 
     def slots(self) -> set[int]:
-        return {y for x in self.cards for y in x.slots}
+        return {slot for card in self.cards for slot in card.slots}
 
     def validate(self) -> None:
         if self.num_slots == 0 or not self.cards:
@@ -232,16 +274,17 @@ class CardImageCollection:
     # endregion
 
 
-@attr.s
+@attr.s(frozen=True)  # freezing so these can be hashed
 class Details:
     quantity: int = attr.ib(default=0)
     stock: str = attr.ib(default=constants.Cardstocks.S30.value)
     foil: bool = attr.ib(default=False)
+    allowed_to_exceed_project_max_size: bool = attr.ib(default=False)
 
     # region initialisation
 
     def validate(self) -> None:
-        if self.quantity > constants.PROJECT_MAX_SIZE:
+        if (not self.allowed_to_exceed_project_max_size) and self.quantity > constants.PROJECT_MAX_SIZE:
             raise ValidationException(
                 f"Order quantity {self.quantity} larger than maximum size of {bold(constants.PROJECT_MAX_SIZE)}!"
             )
@@ -270,7 +313,7 @@ class Details:
         stock = details_dict[constants.DetailsTags.stock].text or constants.Cardstocks.S30
         foil: bool = details_dict[constants.DetailsTags.foil].text == "true"
 
-        details = cls(quantity=quantity, stock=stock, foil=foil)
+        details = cls(quantity=quantity, stock=stock, foil=foil, allowed_to_exceed_project_max_size=True)
         return details
 
     # endregion
@@ -283,17 +326,122 @@ class CardOrder:
     fronts: CardImageCollection = attr.ib(default=None)
     backs: CardImageCollection = attr.ib(default=None)
 
-    # region logging
+    def is_combinable(self, other: "CardOrder") -> bool:
+        return self.details.foil == other.details.foil and self.details.stock == other.details.stock
 
-    def print_order_overview(self) -> None:
-        if self.name is not None:
-            print(f"Successfully parsed card order: {bold(self.name)}")
-        print(
-            f"Your order has a total of {bold(self.details.quantity)} cards."
-            f"\n{bold(self.details.stock)} cardstock ({bold('foil' if self.details.foil else 'nonfoil')}).\n "
+    def combine(self, other: "CardOrder") -> "CardOrder":
+        """
+        Combine `self` and `other` into a new `CardOrder`.
+        Orders must be "compatible" in order to be combined - i.e. they must share the same finish configuration.
+        Where both orders have the same single cardback, we continue to treat the cardback as a singleton
+        because this enables a large time saving optimisation when auto-filling the order into MakePlayingCards.
+        """
+
+        # users should never hit this assertion because we group by details before combining
+        assert self.is_combinable(other=other)
+        return CardOrder(
+            # assume that stock and foil finish are identical because the two orders are combinable
+            details=Details(
+                foil=self.details.foil,
+                stock=self.details.stock,
+                quantity=self.details.quantity + other.details.quantity,
+                allowed_to_exceed_project_max_size=True,
+            ),
+            fronts=self.fronts.combine(other.fronts),
+            backs=self.backs.combine(other.backs),
         )
 
-    # endregion
+    def get_project_sizes(self) -> list[int]:
+        naive_split = f"Split every {constants.PROJECT_MAX_SIZE} cards"
+        interactive_split = "Let me specify how to split the cards"
+        questions = {
+            "type": "list",
+            "name": "split_choices",
+            "message": (
+                f"Your project/s with cardstock {self.details.stock} ({'' if self.details.foil else 'non-'}foil)\n"
+                f"contain {self.details.quantity} cards, which exceeds the maximum project size of "
+                f"{constants.PROJECT_MAX_SIZE}.\n"
+                f"How should the project/s be split to fit within {constants.PROJECT_MAX_SIZE} cards at most?"
+            ),
+            "choices": [naive_split, interactive_split],
+        }
+        answers = prompt(questions)
+        split_method = answers["split_choices"]
+
+        # assume naive split
+        remainder = self.details.quantity % constants.PROJECT_MAX_SIZE
+        project_sizes = [constants.PROJECT_MAX_SIZE] * (
+            (self.details.quantity - remainder) // constants.PROJECT_MAX_SIZE
+        )
+        if remainder:
+            project_sizes.append(remainder)
+
+        if split_method == interactive_split:
+            # ask user to specify their desired splits
+            while True:
+                splits_text = input(
+                    f"Enter a comma-separated list of project sizes (each up to {bold(constants.PROJECT_MAX_SIZE)}) "
+                    f"that sum to {bold(self.details.quantity)} and press {bold('Enter')}. "
+                )
+                try:
+                    project_sizes = [int(split_text) for split_text in splits_text.replace(" ", "").split(",")]
+                    for raw_split in project_sizes:
+                        assert 0 < raw_split <= constants.PROJECT_MAX_SIZE
+                    assert sum(project_sizes) == self.details.quantity
+                    break
+                except (ValueError, AssertionError):
+                    print(
+                        f"There was a problem with your proposed splits (perhaps they didn't sum to "
+                        f"{bold(self.details.quantity)}); please try again."
+                    )
+                    pass
+        print(
+            f"The tool will produce {bold(len(project_sizes))} projects! They'll be sized as follows:\n"
+            f"{', '.join(['Project ' + bold(i) + ': ' + bold(project_size) + ' cards' for i, project_size in enumerate(project_sizes, start=1)])}"
+        )
+        return project_sizes
+
+    def split(self) -> list["CardOrder"]:
+        """
+        Split `self` into multiple orders which each meet the PROJECT_MAX_SIZE upper size bound constraint.
+        """
+
+        if self.details.quantity <= constants.PROJECT_MAX_SIZE:
+            return [self]
+
+        raw_splits = self.get_project_sizes()
+
+        # create the split orders
+        split_orders = [
+            CardOrder(
+                details=Details(
+                    quantity=raw_split,
+                    stock=self.details.stock,
+                    foil=self.details.foil,
+                    allowed_to_exceed_project_max_size=False,
+                ),
+                fronts=CardImageCollection(num_slots=raw_split, face=constants.Faces.front),
+                backs=CardImageCollection(num_slots=raw_split, face=constants.Faces.back),
+            )
+            for raw_split in raw_splits
+        ]
+
+        # this translates splits like [100, 150, 100] into splits like [100, 250, 350]
+        splits = [sum(raw_splits[: i + 1]) for i in range(len(raw_splits))]
+
+        # split each card and assign them to their corresponding order
+        for front_card in self.fronts.cards:
+            split_cards = front_card.split(splits)
+            for i, split_card in enumerate(split_cards):
+                if split_card:
+                    split_orders[i].fronts.cards.append(split_card)
+        for back_card in self.backs.cards:
+            split_cards = back_card.split(splits)
+            for i, split_card in enumerate(split_cards):
+                if split_card:
+                    split_orders[i].backs.cards.append(split_card)
+
+        return split_orders
 
     # region initialisation
 
@@ -352,23 +500,93 @@ class CardOrder:
     # region public
 
     @classmethod
-    def from_xml_in_folder(cls) -> "CardOrder":
+    def from_xmls_in_folder(cls) -> list["CardOrder"]:
         """
-        Reads an XML from the current directory, offering a choice if multiple are detected, and populates this
-        object with the contents of the file.
+        Reads some number of XMLs from the current directory, offering a choice if multiple are detected,
+        and populates them with the contents of the selected files.
+        The primary public entry point to this class.
         """
 
-        xml_glob = list(glob(os.path.join(CURRDIR, "*.xml")))
+        xml_glob = sorted(glob(os.path.join(CURRDIR, "*.xml")))
         if len(xml_glob) <= 0:
             input("No XML files found in this directory. Press enter to exit.")
             sys.exit(0)
         elif len(xml_glob) == 1:
-            file_name = xml_glob[0]
+            file_names = [xml_glob[0]]
         else:
-            xml_select_string = "Multiple XML files found. Please select one for this order: "
-            questions = {"type": "list", "name": "xml_choice", "message": xml_select_string, "choices": xml_glob}
-            answers = InquirerPy.prompt(questions)
-            file_name = answers["xml_choice"]
-        return cls.from_file_name(file_name)
+            xml_select_string = (
+                "Multiple XML files found. Please select any number of them to process.\n"
+                "Select a file by pressing Space, then confirm your selection by pressing Enter."
+            )
+            questions = {
+                "type": "list",
+                "name": "xml_choice",
+                "message": xml_select_string,
+                "choices": xml_glob,
+                "multiselect": True,
+            }
+            answers = prompt(questions)
+            file_names = answers["xml_choice"]
+        return [cls.from_file_name(file_name) for file_name in file_names]
+
+    @classmethod
+    def from_multiple_orders(cls, orders: list["CardOrder"]) -> "CardOrder":
+        """
+        Flatten some number of orders into a single `CardOrder`.
+        """
+
+        assert len(orders) > 0, "Attempted to produce a CardOrder from multiple CardOrders but none were given!"
+        return reduce(lambda a, b: a.combine(b), orders)
+
+    def print_order_overview(self) -> None:
+        if self.name is not None:
+            print(f"Successfully parsed project: {bold(self.name)}")
+        print(
+            f"This project has a total of {bold(self.details.quantity)} cards.\n"
+            f"{bold(self.details.stock)} cardstock ({bold('foil' if self.details.foil else 'nonfoil')}). "
+        )
 
     # endregion
+
+
+def aggregate_and_split_orders(
+    orders: list[CardOrder], target_site: constants.TargetSites, combine_orders: bool
+) -> list[CardOrder]:
+    """
+    Interactively aggregate multiple card orders into consolidated orders.
+
+    This occurs in two phases:
+    1. Combinable orders (orders that share the same finish settings) have their image collections merged.
+        * Note: the complexity here is handling different cardbacks between the orders
+        * This means that if any orders to be combined have different cardbacks,
+          we need to apply the cardback for each project to each slot rather than one slot.
+        * Combined orders can have their image collections spill over the max project size.
+    2. Split the combined orders back out into multiple orders such that they fit into brackets.
+        * For example, combining three 408 card projects will yield two 612 card projects.
+        * Note: the complexity here is optimising for cost across brackets in the target site.
+          I see users wanting to do this in three ways:
+            1. Naively split on max project size (612),
+            2. Read pricing data from the target site, perform some calculations, and suggest the optimal
+               splits for minimising costs,
+                 * Note: out of scope at the moment.
+            3. Allow users to type in their desired splits.
+    """
+
+    if len(orders) == 1:
+        return orders
+
+    def key(order: CardOrder) -> int:
+        return hash((order.details.foil, order.details.stock))
+
+    aggregated_orders = orders
+    if combine_orders:
+        aggregated_orders = [
+            CardOrder.from_multiple_orders(list(grouped_orders))
+            for _, grouped_orders in groupby(sorted(orders, key=key), key=key)
+        ]
+    aggregated_and_split_orders = [
+        aggregated_and_split_order
+        for aggregated_order in aggregated_orders
+        for aggregated_and_split_order in aggregated_order.split()
+    ]
+    return aggregated_and_split_orders
