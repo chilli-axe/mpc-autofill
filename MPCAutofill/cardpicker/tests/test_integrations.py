1241f0820e7a57c6bf4dec9c67b3eb3bc217e1bb
Nicholas de Paola <3079166+ndepaola@users.noreply.github.com>
2024-04-25 21:43:08 +1000

    some light test refactoring

--- MPCAutofill/cardpicker/tests/test_integrations.py
+++ MPCAutofill/cardpicker/tests/test_integrations.py
@@ -17,24 +17,21 @@ class TestGetIntegration:
 class TestMTGIntegration:
     # region constants
 
-    class Decks(str, Enum):
+    class Decks(Enum):
         # all of these decks have 4x brainstorm, 3x past in flames, and 1x delver of secrets // insectile aberration
-        AETHER_HUB = "https://aetherhub.com/Deck/test-796905"
+        AETHERHUB = "https://aetherhub.com/Deck/test-796905"
         ARCHIDEKT = "https://archidekt.com/decks/3380653"
         ARCHIDEKT_WITH_HASH = "https://archidekt.com/decks/3380653#test"
         ARCHIDEKT_WITH_SLASH = "https://archidekt.com/decks/3380653/test"
-        CUBE_COBRA = "https://cubecobra.com/cube/overview/2fj4"
-        DECK_STATS = "https://deckstats.net/decks/216625/2754468-test"
-        MAGIC_VILLE = "https://magic-ville.com/fr/decks/showdeck?ref=948045"
-        MANA_STACK = "https://manastack.com/deck/test-426"
+        CUBECOBRA = "https://cubecobra.com/cube/overview/2fj4"
+        DECKSTATS = "https://deckstats.net/decks/216625/2754468-test"
+        MAGICVILLE = "https://magic-ville.com/fr/decks/showdeck?ref=948045"
+        MANASTACK = "https://manastack.com/deck/test-426"
         MOXFIELD = "https://www.moxfield.com/decks/D42-or9pCk-uMi4XzRDziQ"
-        MTG_GOLDFISH = "https://www.mtggoldfish.com/deck/5149750"
+        MTGGOLDFISH = "https://www.mtggoldfish.com/deck/5149750"
         SCRYFALL = "https://scryfall.com/@mpcautofill/decks/71bb2d40-c922-4a01-a63e-7ba2dde29a5c"
-        TAPPED_OUT = "https://tappedout.net/mtg-decks/09-10-22-DoY-test"
-        TCG_PLAYER = "https://decks.tcgplayer.com/magic/standard/mpc-autofill/test/1398367"
-
-        def __str__(self):
-            return self.value
+        TAPPEDOUT = "https://tappedout.net/mtg-decks/09-10-22-DoY-test"
+        TCGPLAYER = "https://decks.tcgplayer.com/magic/standard/mpc-autofill/test/1398367"
 
     # endregion
 
@@ -46,8 +43,8 @@ class TestMTGIntegration:
     def test_get_meld_pairs(self):
         assert len(MTG.get_meld_pairs()) > 0
 
-    @pytest.mark.parametrize("url", list(Decks))
-    def test_valid_url(self, client, django_settings, snapshot, url):
+    @pytest.mark.parametrize("url", [item.value for item in Decks], ids=[item.name.lower() for item in Decks])
+    def test_valid_url(self, client, django_settings, snapshot, url: str):
         decklist = MTG.query_import_site(url)
         assert Counter(decklist.splitlines()) == snapshot
 
