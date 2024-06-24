8743da768ce3c133855f28ecfad4adb32b2b624e
Nicholas de Paola <ndepaola97@gmail.com>
2024-06-24 23:42:46 +1000

    use python 3.9 typing here and fix pre-commit issues

--- desktop-tool/src/web_server.py
+++ desktop-tool/src/web_server.py
@@ -1,13 +1,19 @@
+"""
+This module spins up a simple web server for serving the landing page for the tool
+(guiding the user back to the desktop tool to answer another question before it can proceed).
+"""
+
 import logging
 import threading
 from http import server
 from pathlib import Path
+from typing import Union
 
 from src import constants
 
 
 class _Handler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
+    def do_GET(self) -> None:
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
@@ -15,13 +21,13 @@ class _Handler(server.BaseHTTPRequestHandler):
             Path(__file__).joinpath("../..").joinpath(constants.POST_LAUNCH_HTML_FILENAME).resolve().read_bytes()
         )
 
-    def log_request(self, code: int | str = "-", size: int | str = "-") -> None:
+    def log_request(self, code: Union[int, str] = "-", size: Union[int, str] = "-") -> None:
         # Silence the request log.
         pass
 
 
 class WebServer:
-    def __init__(self):
+    def __init__(self) -> None:
         self._server = server.ThreadingHTTPServer(("", 0), _Handler)
         self._thread = threading.Thread(target=self._server.serve_forever)
         self._thread.start()
@@ -30,6 +36,6 @@ class WebServer:
     def server_url(self) -> str:
         return f"http://localhost:{self._server.socket.getsockname()[1]}/"
 
-    def __del__(self):
+    def __del__(self) -> None:
         self._server.shutdown()
         self._thread.join()
