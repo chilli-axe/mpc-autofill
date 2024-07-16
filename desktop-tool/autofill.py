0a0202c656c0d263c99a9c57284afd8361263321
Nicholas de Paola <ndepaola97@gmail.com>
2024-07-16 22:30:21 +1000

    follow the docs for specifying compilation mode in comments

--- desktop-tool/autofill.py
+++ desktop-tool/autofill.py
@@ -1,3 +1,10 @@
+# Compilation mode, standalone everywhere, except on macOS there app bundle
+# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
+#    nuitka-project: --onefile
+# nuitka-project-if: {OS} == "Darwin":
+#    nuitka-project: --standalone
+#    nuitka-project: --macos-create-app-bundle
+
 import logging
 import os
 import sys
