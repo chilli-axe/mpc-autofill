3cbfec4c11d33f437bdce140d013739804088749
Nicholas de Paola <ndepaola97@gmail.com>
2025-02-02 08:15:12 +1000

    visual flair

--- frontend/src/components/BlurrableRow.tsx
+++ frontend/src/components/BlurrableRow.tsx
@@ -7,5 +7,6 @@ interface BlurrableRowProps {
 
 export const BlurrableRow = styled(Row)<BlurrableRowProps>`
   filter: ${(props) => (props.disabled === true ? "blur(8px)" : undefined)};
+  transition: filter 0.2s;
   pointer-events: ${(props) => (props.disabled === true ? "none" : undefined)};
 `;
