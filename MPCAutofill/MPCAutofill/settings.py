6cc0dd4ecfff25d7f574686db13c90de4dba051f
Nicholas de Paola <ndepaola97@gmail.com>
2025-05-23 21:42:04 +1000

    fixes identified in staging server

--- MPCAutofill/MPCAutofill/settings.py
+++ MPCAutofill/MPCAutofill/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
