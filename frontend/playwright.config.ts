11c14f206c760e63e40c65e903a41aa24fe3d201
Nicholas de Paola <ndepaola97@gmail.com>
2026-01-12 17:05:32 +1000

    :/

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Always run tests in parallel. Default is to not parallelise in CI, this seems nuts to me. */
+  workers: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
