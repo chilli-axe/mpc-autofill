6d50659fac6cfb3ed67cfe45d693c5f64ca709db
Nicholas de Paola <ndepaola97@gmail.com>
2026-04-26 21:38:24 +1000

    consolidate canonical card data import mgmt commands

--- MPCAutofill/cardpicker/management/commands/import_canonical_card_data.py
+++ MPCAutofill/cardpicker/management/commands/import_canonical_card_data.py
@@ -6,10 +6,11 @@ from cardpicker.integrations.integrations import get_configured_game_integration
 
 
 class Command(BaseCommand):
-    help = "Imports canonical expansions for the configured game integration."
+    help = "Imports canonical artists and cards for the configured game integration."
 
     def handle(self, *args: Any, **kwargs: Any) -> None:
         game_integration = get_configured_game_integration()
         if game_integration is None:
             raise Exception("No game integration is configured.")
         game_integration.import_canonical_expansions()
+        game_integration.import_canonical_cards_and_artists()
