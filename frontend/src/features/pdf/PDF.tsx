9021e3ec68e0678b6356458a4aa5faf28ba6604d
Nicholas de Paola <ndepaola97@gmail.com>
2026-06-02 22:41:54 +1000

    Revert "centre cards vertically within page"
    This reverts commit a62ca80a39b21c2b378f356413ec75c65eab487b.

--- frontend/src/features/pdf/PDF.tsx
+++ frontend/src/features/pdf/PDF.tsx
@@ -963,8 +963,6 @@ export const PDF = (props: PDFProps) => {
             paddingBottom: props.pageMarginBottomMM + "mm",
             paddingLeft: props.pageMarginLeftMM + "mm",
             paddingRight: props.pageMarginRightMM + "mm",
-            display: "flex",
-            justifyContent: "center",
           }}
         >
           <PageComponent pageWidthMM={size.width} pageHeightMM={size.height} />
